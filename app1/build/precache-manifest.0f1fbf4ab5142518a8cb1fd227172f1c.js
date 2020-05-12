f1e070693ff474e01a89b1d287687e96c8c1548c
pat-white-code <pat.k.white@gmail.com>
2020-05-12 15:14:46 -0500

    adds new build

---
+++ app1/build/precache-manifest.0f1fbf4ab5142518a8cb1fd227172f1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bca712a428e1dc6615f38a378a2b8b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce56351ae2c5536eb1b8",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6c411fe0e4e493cdbb54",
+    "url": "/static/js/2.9aa87651.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9aa87651.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce56351ae2c5536eb1b8",
+    "url": "/static/js/main.2c922c1c.chunk.js"
+  },
+  {
+    "revision": "f97e01b6b19b892529fb",
+    "url": "/static/js/runtime-main.19d513ca.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
